dc1d5fcb7e48c6fe908c40c53872b1543d7a9cf2
Sergey Zaligin <webdevjsphp@gmail.com>
2018-09-30 16:49:49 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 
 import Layout from "./hoc/Layout/Layout";
+import Quiz from "./containers/Quiz/Quiz";
 
 class App extends Component {
   render() {
-    return <Layout>Hello world</Layout>;
+    return (
+      <Layout>
+        <Quiz />
+      </Layout>
+    );
   }
 }
 
